f7073517c137172f20f9694a956e98744594994a
Nacho Díez <idiegar2502@g.educaand.es>
2024-10-23 23:47:08 +0200

    Segundos y minutos :D

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int seg = sc.nextInt();
+        int min = 0;
+        boolean mYs = true;
+
+        if(seg<60 && seg>0){
+            System.out.println( min+" minutos y "+seg+" segundos");
+        }else if (seg>=60){
+            boolean mYS = true;
+        }else{
+            System.out.println("Tiempo incorrecto, posible error numeros negativos o 0 segundos");
+        }
+
+        while (mYs && seg>=60) {
+            min = min+1;
+            seg = seg-60;
+        }
+        System.out.println(min+" minutos y "+seg+" segundos");
+    }
+}
